4fcd5df714db27dd2a978c16d067b6e9d5d33579
Galus Klemens <galus.klemens@gmail.com>
2023-10-27 12:36:07 +0200

    feat!: an awsome feature
    this is a test

--- main.go
+++ main.go
@@ -12,8 +12,9 @@ func main() {
 	dat, _ := ioutil.ReadFile(commitFile)
 	commit := string(dat)
 	isValid := gomit.Check_commit_size(commit)
+	isValid2 := gomit.Check_commit_lint(commit)
 	gomit.Check_commit_lint(commit)
-	if isValid {
+	if isValid && isValid2 {
 		os.Exit(0)
 	} else {
 		os.Exit(1)
