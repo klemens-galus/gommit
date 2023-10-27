b7e06ea575e197a697042d7ee82a24695584b8aa
Galus Klemens <galus.klemens@gmail.com>
2023-10-27 14:35:56 +0200

    fix: change error message

--- commit/error_gestion.go
+++ commit/error_gestion.go
@@ -15,7 +15,7 @@ func show_exemple(){
 	fmt.Println("The right commit format is :")
 	fmt.Println("	"+string(colorBlue)+"<type>"+string(colorMagenta)+"(<scope>)"+string(colorReset)+": "+string(colorCyan)+"<short summary>"+string(colorReset))
 	fmt.Println(string(colorBlue),"type : the commit type"+string(colorReset))
-	fmt.Println(string(colorMagenta),"scope : to give more contect to a commit (optional)"+string(colorReset))
+	fmt.Println(string(colorMagenta),"scope : to give more contect to your commit (optional)"+string(colorReset))
 	fmt.Println(string(colorCyan),"short summary : a short summary of the code changes"+string(colorReset))
 	fmt.Println("Exemples :")
 	fmt.Println(string(colorGreen)+"	fix: change payload of login route	is a good commit message"+string(colorReset))
