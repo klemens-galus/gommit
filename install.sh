953f010c526d6986d62a1f638059719cb8b09204
Galus Klemens <galus.klemens@gmail.com>
2023-10-30 17:53:37 +0100

    build: fix installation script

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ fi
 
 if [ -z "$(uname -a | grep -e Linux)" ]; then
     mkdir -p .git/myhooks
-    curl https://github.com/klemens-galus/gommit/releases/download/1.0.0/gommit_linux_amd64 -o prepare-commit-msg
+    curl https://github.com/klemens-galus/gommit/releases/download/1.0.0/gommit_linux_amd64 -o .git/myhooks/prepare-commit-msg
     chmod +x .git/myhooks/prepare-commit-msg
 fi 
 
 if [ -z "$(uname -a | grep -e MINGW64)" ]; then
     mkdir -p .git/myhooks
-    curl https://github.com/klemens-galus/gommit/releases/download/1.0.0/gommit_windows_amd64 -o prepare-commit-msg
-    chmod +x .git/myhooks/prepare-commit-msg
+    curl https://github.com/klemens-galus/gommit/releases/download/1.0.0/gommit_windows_amd64 -o .git/myhooks/prepare-commit-msg
+    chmod +x prepare-commit-msg
 fi 
 
 
