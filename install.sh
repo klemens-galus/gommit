07e946e3c67a2ee4280de11ace4f90bdeaed4a6f
Galus Klemens <galus.klemens@gmail.com>
2023-10-30 17:36:47 +0100

    build: add installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
-git config --local core.hooksPath .git/myhooks
-rm -fr .git/myhooks/
-mkdir -p .git/myhooks
-cp hooks/* .git/myhooks/
+os=$(uname -a | grep -e Linux -e MINGW64)
 
-chmod +x .git/myhooks/prepare-commit-msg
+echo $os
+
+if [ -z "${VAR}" ]; then
+    echo "OS is not available"
+    exit 1
+fi
+
+if [ -z "$(uname -a | grep -e Linux)" ]; then
+    mkdir -p .git/myhooks
+    curl https://github.com/klemens-galus/gommit/releases/download/1.0.0/gommit_linux_amd64 -o prepare-commit-msg
+    chmod +x .git/myhooks/prepare-commit-msg
+fi 
+
+if [ -z "$(uname -a | grep -e MINGW64)" ]; then
+    mkdir -p .git/myhooks
+    curl https://github.com/klemens-galus/gommit/releases/download/1.0.0/gommit_windows_amd64 -o prepare-commit-msg
+    chmod +x .git/myhooks/prepare-commit-msg
+fi 
+
+
+# chmod +x .git/myhooks/prepare-commit-msg
