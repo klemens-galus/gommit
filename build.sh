bec61214f6518f93eee2c761ca2594f94110fcbc
Klemens Galus <galus.klemens@gmail.com>
2023-10-30 22:16:36 +0100

    build: fix build script for windows

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 GOOS=darwin GOARCH=arm64 go build -o bin/gommit_darwin_arm64
 GOOS=darwin GOARCH=amd64 go build -o bin/gommit_darwin_amd64
 
-GOOS=windows GOARCH=amd64 go build -o bin/gommit_windows_amd64
+GOOS=windows GOARCH=amd64 go build -o bin/gommit_windows_amd64.exe
 
 GOOS=linux GOARCH=arm64 go build -o bin/gommit_linux_amd64
 GOOS=linux GOARCH=amd64 go build -o bin/gommit_linux_amd64
