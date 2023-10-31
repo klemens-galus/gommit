5cc9d69adebe62f74e288482a37a82c06b5874fe
Klemens Galus <galus.klemens@gmail.com>
2023-10-31 02:13:59 +0100

    build: add mac amd and arm

--- install_mac_AMD64.sh
+++ install_mac_AMD64.sh
@@ -1,5 +1,6 @@
 mkdir -p .git/myhooks
 curl  -L -o ./.git/myhooks/gommit_darwin_amd64.tar https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_darwin_amd64.tar
 tar -xf .git/myhooks/gommit_darwin_amd64.tar -C .git/myhooks/
+mv .git/myhooks/gommit_darwin_amd64 .git/myhooks/prepare-commit-msg
 chmod +x .git/myhooks/prepare-commit-msg
 git config --local core.hooksPath .git/myhooks
--- install_mac_ARM64.sh
+++ install_mac_ARM64.sh
@@ -1,7 +1,7 @@
 mkdir -p .git/myhooks
 curl  -L -o ./.git/myhooks/gommit_darwin_arm64.tar https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_darwin_arm64.tar
 tar -xf .git/myhooks/gommit_darwin_arm64.tar -C .git/myhooks/
-chmod +x .git/myhooks/prepare-commit-msg
+mv .git/myhooks/gommit_darwin_arm64 .git/myhooks/prepare-commit-msg
 git config --local core.hooksPath .git/myhooks
 
 
