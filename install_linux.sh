7b353e75469bc5d8e2a3361a146a162ba5989fa1
Klemens Galus <galus.klemens@gmail.com>
2023-10-31 02:06:41 +0100

    build: change file name for linux

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,5 @@
 mkdir -p .git/myhooks
-curl  -L -o ./.git/myhooks/prepare-commit-msg.tar https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_linux_amd64.tar
+curl  -L -o ./.git/myhooks/gommit_linux_amd64.tar https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_linux_amd64.tar
 tar -xf .git/myhooks/gommit_linux_amd64.tar -C .git/myhooks/
 chmod +x .git/myhooks/prepare-commit-msg.tar
 git config --local core.hooksPath .git/myhooks
