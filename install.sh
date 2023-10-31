b3c264c8b698d0d50c4498562f2a8fe8664477b2
Klemens Galus <galus.klemens@gmail.com>
2023-10-31 01:45:49 +0100

    build: download and unzip

--- install.sh
+++ install.sh
@@ -9,14 +9,15 @@ fi
 
 if [ -z "$(uname -a | grep -e Linux)" ]; then
     mkdir -p .git/myhooks
-    curl https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_linux_amd64 -o .git/myhooks/prepare-commit-msg
+    curl  -L -o ./.git/myhooks/prepare-commit-msg.zip https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_linux_amd64.zip
+    tar -xf .git/myhooks/gommit_windows_amd64.zip -C .git/myhooks/
     chmod +x .git/myhooks/prepare-commit-msg
     git config --local core.hooksPath .git/myhooks
 fi 
 
 if [ -z "$(uname -a | grep -e MINGW64)" ]; then
     mkdir -p .git/myhooks
-    curl https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_windows_amd64.zip -o .git/myhooks/gommit_windows_amd64.zip
+    curl  -L -o ./.git/myhooks/gommit_windows_amd64.zip https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_windows_amd64.zip
     tar -xf .git/myhooks/gommit_windows_amd64.zip -C .git/myhooks/
     mv .git/myhooks/gommit_windows_amd64.exe .git/myhooks/prepare-commit-msg.exe
     rm -f .git/myhooks/*.zip
