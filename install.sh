4decff07776fbd95adc991cf1987f096062aec0f
Klemens Galus <galus.klemens@gmail.com>
2023-10-31 01:52:24 +0100

    build: download and unzip

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ -z "$(uname -a | grep -e Linux)" ]; then
     mkdir -p .git/myhooks
     curl  -L -o ./.git/myhooks/prepare-commit-msg.tar https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_linux_amd64.tar
     tar -xf .git/myhooks/gommit_linux_amd64.tar -C .git/myhooks/
-    chmod +x .git/myhooks/prepare-commit-msg
+    chmod +x .git/myhooks/prepare-commit-msg.tar
     git config --local core.hooksPath .git/myhooks
 fi 
 
