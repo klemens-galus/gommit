d0befb88b805c41f66661a9c28a3ae257e4b432a
Galus Klemens <galus.klemens@gmail.com>
2023-11-02 14:47:09 +0100

    build: add ldflags for debugmode

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-GOOS=darwin GOARCH=arm64 go build -o bin/gommit_darwin_arm64
-GOOS=darwin GOARCH=amd64 go build -o bin/gommit_darwin_amd64
+GOOS=darwin GOARCH=arm64 go build -o bin/gommit_darwin_arm64 -ldflags "-X main.debugMode=false"
+GOOS=darwin GOARCH=amd64 go build -o bin/gommit_darwin_amd64 -ldflags "-X main.debugMode=false"
 
-GOOS=windows GOARCH=amd64 go build -o bin/gommit_windows_amd64.exe
+GOOS=windows GOARCH=amd64 go build -o bin/gommit_windows_amd64.exe -ldflags "-X main.debugMode=false"
 
-GOOS=linux GOARCH=arm64 go build -o bin/gommit_linux_amd64
-GOOS=linux GOARCH=amd64 go build -o bin/gommit_linux_amd64
+GOOS=linux GOARCH=arm64 go build -o bin/gommit_linux_amd64 -ldflags "-X main.debugMode=false"
+GOOS=linux GOARCH=amd64 go build -o bin/gommit_linux_amd64 -ldflags "-X main.debugMode=false"
 # GOOS=linux GOARCH=amd64 go build -o bin/app-amd64-linux app.go
