50b11f8f995429fc54de0ae61b2bd22a80779d8c
Klemens Galus <galus.klemens@gmail.com>
2023-11-03 00:34:28 +0100

    build: fix bin name

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ GOOS=darwin GOARCH=amd64 go build -o bin/gommit_darwin_amd64 -ldflags "-X main.d
 
 GOOS=windows GOARCH=amd64 go build -o bin/gommit_windows_amd64.exe -ldflags "-X main.debugMode=false"
 
-GOOS=linux GOARCH=arm64 go build -o bin/gommit_linux_amd64 -ldflags "-X main.debugMode=false"
+GOOS=linux GOARCH=arm64 go build -o bin/gommit_linux_arm64 -ldflags "-X main.debugMode=false"
 GOOS=linux GOARCH=amd64 go build -o bin/gommit_linux_amd64 -ldflags "-X main.debugMode=false"
 # GOOS=linux GOARCH=amd64 go build -o bin/app-amd64-linux app.go
