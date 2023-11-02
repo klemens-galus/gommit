6ab97a3362c651aeae21224ea7acea7b644c45d6
Klemens Galus <galus.klemens@gmail.com>
2023-11-02 17:06:48 +0100

    build: change download path

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,5 @@
 mkdir -p .git/myhooks
-curl  -L -o ./.git/myhooks/gommit_linux_amd64.tar https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_linux_amd64.tar
+curl  -L -o ./.git/myhooks/gommit_linux_amd64.tar https://github.com/klemens-galus/gommit/releases/download/1.0.5/gommit_linux_amd64.tar
 tar -xf .git/myhooks/gommit_linux_amd64.tar -C .git/myhooks/
 mv .git/myhooks/gommit_linux_amd64 .git/myhooks/prepare-commit-msg
 git config --local core.hooksPath .git/myhooks
--- install_windows.sh
+++ install_windows.sh
@@ -1,6 +1,6 @@
 
 mkdir -p .git/myhooks
-curl  -L -o ./.git/myhooks/gommit_windows_amd64.zip https://github.com/klemens-galus/gommit/releases/download/1.0.2/gommit_windows_amd64.zip
+curl  -L -o ./.git/myhooks/gommit_windows_amd64.zip https://github.com/klemens-galus/gommit/releases/download/1.0.5/gommit_windows_amd64.zip
 tar -xf .git/myhooks/gommit_windows_amd64.zip -C .git/myhooks/
 mv .git/myhooks/gommit_windows_amd64.exe .git/myhooks/prepare-commit-msg.exe
 rm -f .git/myhooks/*.zip
